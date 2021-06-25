4e192bfd3cfedcac9e9fc32c844caa52567cee35
GabrielGrgich <72478000+GabrielGrgich@users.noreply.github.com>
2021-06-24 18:02:21 -0700

    Add files via upload

---
+++ screens/homeScreen.js
@@ -0,0 +1,15 @@
+
+import React,{Component} from "react"
+import {StyleSheet, View, Text, TextInput, TouchableOpacity} from "react-native"
+import {Header} from "react-native-elements"
+
+export default class HomeScreen extends Component{
+    constructor(){
+    super();
+    this.state = {
+    text:'',
+    
+    }
+
+    }
+}
